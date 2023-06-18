7345dae0bcf70bf6dc70104c3d14c4c90e4e05d6
hukutatu <127284708+hukutatu@users.noreply.github.com>
2023-06-18 11:28:09 +0000

    db修正

--- db/migrate/20230606091707_create_recipes.rb
+++ db/migrate/20230606091707_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
       t.integer :user_id
-      t.text :capion,   null: false, default: ""
+      t.text :capion,   null: false
       t.string :title,  null: false, default: ""
       t.timestamps
     end
